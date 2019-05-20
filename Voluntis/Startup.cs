9ed39836bc77c8515633fa8bfcb3a57ee3c55b4f
Vincent <vleclerc@inow.fr>
2019-05-20 15:34:19 +0200

    ajout de la conf de base

--- Voluntis/Startup.cs
+++ Voluntis/Startup.cs
@@ -5,16 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+// https://github.com/bart120/Voluntis
 
 namespace Voluntis
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true);
+
+            this.Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +41,22 @@ namespace Voluntis
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            // Middleware pour les fichiers statics
+            app.UseStaticFiles();
+
+            app.UseMvc(ConfigureRoute);
+
+            /*app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
-            });
+            });*/
+        }
+
+        private void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(
+                name: "Default",
+                template: "{controller}/{action}/{id?}");
         }
     }
 }
