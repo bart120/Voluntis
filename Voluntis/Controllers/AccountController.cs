41fca8d2a53d69a22f19ba1e898615fe377abcd2
Vincent <vleclerc@inow.fr>
2019-05-21 14:28:20 +0200

    ajout des validateurs, des champs de saisie, relation model <> view

--- Voluntis/Controllers/AccountController.cs
+++ Voluntis/Controllers/AccountController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Voluntis.Models;
 
 namespace Voluntis.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //save bdd
+            }
             return View();
         }
     }
--- Voluntis/Models/RegisterViewModel.cs
+++ Voluntis/Models/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,34 @@ namespace Voluntis.Models
 {
     public class RegisterViewModel
     {
+        [Display(Name = "Nom", Prompt = "votre nom")]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [StringLength(30, ErrorMessage = "Le champ {0} ne doit pas dépasser {1} caractères.")]
         public string Lastname { get; set; }
 
+        [Display(Name = "Prénom", Prompt = "votre prénom")]
         public string Firstname { get; set; }
 
+        [Display(Name = "Mail", Prompt = "votre mail")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [EmailAddress(ErrorMessage = "Format incorrect.")]
         public string Email { get; set; }
 
+        [Display(Name = "Mot de passe", Prompt = "votre mot de passe")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$", ErrorMessage = "Mot de passe invalide.")]
         public string Password { get; set; }
 
+        [Display(Name = "Confirmation", Prompt = "confirmation de votre mot de passe")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "La confirmation n'est pas identique.")]
         public string ConfirmPassword { get; set; }
 
+        [Display(Name = "Date de naissance", Prompt = "votre date de naissance")]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
     }
 }
