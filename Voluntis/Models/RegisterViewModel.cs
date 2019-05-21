2987da4ba8d4afdd899d1580929b463af058ec82
Vincent <vleclerc@inow.fr>
2019-05-21 11:46:37 +0200

    ajout du model User et du Viewmodel Register

---
+++ Voluntis/Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Voluntis.Models
+{
+    public class RegisterViewModel
+    {
+        public string Lastname { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
---
+++ Voluntis/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Voluntis.Models
+{
+    public class User : IdentityUser
+    {
+        
+        public string Lastname { get; set; }
+
+        public string Firstname { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
