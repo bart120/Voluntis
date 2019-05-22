9206929c8371bf5b3e9341f6ea542dd70d828da3
Vincent <vleclerc@inow.fr>
2019-05-22 14:51:39 +0200

    utilisation model, viewbag et dbcontext

--- Voluntis/Areas/Backoffice/Controllers/DashboardController.cs
+++ Voluntis/Areas/Backoffice/Controllers/DashboardController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Voluntis.Data;
 
 namespace Voluntis.Areas.Backoffice.Controllers
 {
     [Area("Backoffice")]
     public class DashboardController : Controller
     {
+        private readonly VoluntisDbContext voluntisDbContext;
+        public DashboardController(VoluntisDbContext voluntisDbContext)
+        {
+            this.voluntisDbContext = voluntisDbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["Users"] = voluntisDbContext.Users;
+
+            var categories = voluntisDbContext.Categories.ToList();
+            return View(categories);
         }
     }
 }
