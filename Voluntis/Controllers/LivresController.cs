f86bcc0f7647757ea813daa6c4215ea51f436dcb
Vincent <vleclerc@inow.fr>
2019-05-23 16:28:20 +0200

    ajout d'une WebAPI

---
+++ Voluntis/Controllers/LivresController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Voluntis.Data;
+using Voluntis.Models;
+
+namespace Voluntis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivresController : ControllerBase
+    {
+        private readonly VoluntisDbContext _context;
+
+        public LivresController(VoluntisDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Livres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetToto()
+        {
+            return await _context.Books.Include(x => x.Category).ToListAsync();
+        }
+
+        // GET: api/Livres/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        // PUT: api/Livres/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Livres
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.ID }, book);
+        }
+
+        // DELETE: api/Livres/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return book;
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.ID == id);
+        }
+    }
+}
